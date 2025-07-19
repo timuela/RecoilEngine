21bc6b975c037a448095f8273afd7b962d131b56
lostsquirrel1 <lostsquirrel43@gmail.com>
2025-07-19 14:27:00 +0100

    Update Synced Work Thread Pinning (#2377)
    * Fix bad goal performance spike
    * Pinning sync'ed worker threads, not unsynced worker threads.
    * Add config support
    * add masking to prefer best cache groups.

--- rts/System/Platform/Threading.h
+++ rts/System/Platform/Threading.h
@@ -12,7 +12,7 @@
 #ifdef __APPLE__
 #include <libkern/OSAtomic.h> // OSAtomicIncrement64
 #endif
-
+#include "CpuTopology.h"
 #include "System/Platform/Win/win32.h"
 #include "System/Threading/SpringThreading.h"
 
@@ -103,6 +103,7 @@ namespace Threading {
 
 	inline bool NativeThreadIdsEqual(const NativeThreadId thID1, const NativeThreadId thID2);
 
+	cpu_topology::ThreadPinPolicy GetChosenThreadPinPolicy();
 
 	/**
 	 * Sets the affinity of the current thread
@@ -125,8 +126,9 @@ namespace Threading {
 	int GetPerformanceCpuCores(); /// performance physical cores only (excluding hyperthreading or efficiency cores)
 	bool HasHyperThreading();
 
-	uint32_t GetSystemAffinityMask();
-	uint32_t GetPreferredMainThreadMask();
+	uint32_t GetSystemAffinityMask(int forThreadCount = std::numeric_limits<int>::max());
+	uint32_t GetPreferredMainThreadMask(uint32_t affinityMask);
+	uint32_t GetOptimalThreadCount();
 
 	/**
 	 * Inform the OS kernel that we are a cpu-intensive task
